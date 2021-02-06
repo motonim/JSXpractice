d332a39cd6fc27442572812be8ac1185955fef26
Jaeri Park <59538964+motonim@users.noreply.github.com>
2021-02-06 17:12:14 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <p>Created by Jaeri Park</p>
+    <p>Copyright {new Date().getFullYear()}</p>
+  </div>,
+  document.getElementById("root")
+);
